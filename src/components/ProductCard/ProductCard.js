b37866186f93892b172b02b4ba2cc495c54f44fd
Adam Głowacz <adaglo524@gmail.com>
2022-06-11 18:28:48 +0200

    prepare front to deploy on heroku with express server, now to start in localhost run npm run startev

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,62 +3,72 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import {Button, CardActionArea, CardActions} from "@mui/material";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 export function ProductCard(props) {
-    return (
-        <Card
-            sx={{
-                margin: props.size === "sm" ? "5px" : "5px",
-                // marginLeft: "10px",
-                // marginRight: "10px",
-                // marginTop: "15px",
-                fontSize: "10px",
-                borderRadius: "20px",
-                boxShadow: "none",
-                maxWidth: props.size === "sm" ? "240px" : "260px",
-                minWidth: "240px",
-                backgroundColor: "#f3f3f3",
-            }}
+  return (
+    <Card
+      sx={{
+        margin: props.size === "sm" ? "5px" : "5px",
+        // marginLeft: "10px",
+        // marginRight: "10px",
+        // marginTop: "15px",
+        fontSize: "10px",
+        borderRadius: "20px",
+        boxShadow: "none",
+        maxWidth: props.size === "sm" ? "240px" : "260px",
+        minWidth: "240px",
+        backgroundColor: "#f3f3f3",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height={props.size === "sm" ? "140px" : "160px"}
+          image={props?.img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h6"
+            sx={{ minHeight: "65px" }}
+            component="div"
+          >
+            {props?.name}
+          </Typography>
+          {/*<Typography variant="body2" color="text.secondary">*/}
+          {/*  */}
+          {/*</Typography>*/}
+        </CardContent>
+      </CardActionArea>
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignSelf: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{ marginLeft: "10px", color: "black" }}
+          variant="h5"
+          color="text.secondary"
         >
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height={props.size === "sm" ? "140px" : "160px"}
-                    image={props.img}
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h6" sx={{minHeight: "65px"}} component="div">
-                        {props.name}
-                    </Typography>
-                    {/*<Typography variant="body2" color="text.secondary">*/}
-                    {/*  */}
-                    {/*</Typography>*/}
-                </CardContent>
-            </CardActionArea>
-            <CardActions
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignSelf: "flex-end",
-                    alignItems: "center",
-                }}
-            >
-                <Typography sx={{marginLeft: "10px", color: "black"}} variant="h5"
-                            color="text.secondary">{props.price} zł</Typography>
-                <Button
-                    size="small"
-                    sx={{
-                        color: "black",
-                        backgroundColor: "white",
-                        borderRadius: "30px",
-                        padding: "10px 25px",
-                    }}
-                >
-                    Dodaj
-                </Button>
-            </CardActions>
-        </Card>
-    );
+          {props?.price} zł
+        </Typography>
+        <Button
+          size="small"
+          sx={{
+            color: "black",
+            backgroundColor: "white",
+            borderRadius: "30px",
+            padding: "10px 25px",
+          }}
+        >
+          Dodaj
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
--- src/components/RecommendedProducts/RecommendedProducts.js
+++ src/components/RecommendedProducts/RecommendedProducts.js
@@ -1,46 +1,54 @@
 import React from "react";
-import {ProductCard} from "../ProductCard/ProductCard";
+import { ProductCard } from "../ProductCard/ProductCard";
 import Box from "@mui/material/Box";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const RecommendedProducts = (props) => {
-    const items = useSelector((state) => state.plants.store)
-    const matches = useMediaQuery("(min-width:1040px)");
+  const items = useSelector((state) => state.plants.store);
+  const matches = useMediaQuery("(min-width:1040px)");
 
-    let itemsRecommended = [];
+  let itemsRecommended = [];
 
-    for (let i = 0; i < 4; i++) {
-        itemsRecommended.push(items[i])
-    }
+  for (let i = 0; i < 4; i++) {
+    itemsRecommended.push(items[i]);
+  }
 
-    return (
-        <Box sx={{marginY: "5rem"}}>
-            <Box style={{display: "flex"}}>
-                <h2>Polecane</h2>
-                <h2
-                    style={{
-                        fontFamily: "NHaasGroteskTXPro",
-                        fontWeight: "lighter",
-                        marginLeft: "6px",
-                    }}
-                >
-                    dla Ciebie
-                </h2>
-            </Box>
-            <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: matches ? "space-between" : "center",
-                    flexWrap: "wrap",
-                }}
-            >
-                {itemsRecommended.map((el) => {
-                    return <Link to={`/sklep/${el.id}`}> <ProductCard name={el.name} price={el.price}
-                                                                      img={el.imgURL}></ProductCard></Link>
-                })}
-            </Box>
-        </Box>
-    );
+  return (
+    <Box sx={{ marginY: "5rem" }}>
+      <Box style={{ display: "flex" }}>
+        <h2>Polecane</h2>
+        <h2
+          style={{
+            fontFamily: "NHaasGroteskTXPro",
+            fontWeight: "lighter",
+            marginLeft: "6px",
+          }}
+        >
+          dla Ciebie
+        </h2>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: matches ? "space-between" : "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {itemsRecommended.map((el) => {
+          return (
+            <Link to={`/sklep/${el?.id}`}>
+              {" "}
+              <ProductCard
+                name={el?.name}
+                price={el?.price}
+                img={el?.imgURL}
+              ></ProductCard>
+            </Link>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 };
